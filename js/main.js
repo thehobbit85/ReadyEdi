ed55e4f131231cf3cee95e725cda83db823fc0fe
thehobbit85 <thehobbit85@gmail.com>
2016-07-17 21:25:16 +0300

    Added animation

--- js/main.js
+++ js/main.js
@@ -7,9 +7,16 @@ $('video').click(function () {
 $(function () {
     // build scene
 
-    var scene = new ScrollMagic.Scene({triggerElement: "#step", triggerHook: 0, duration: "400%"})
+    var scene = new ScrollMagic.Scene({triggerElement: "#step", triggerHook: 0, duration: "1100%"})
     .setPin("#cd-vertical-nav")
     .addTo(controller);
+
+    for (var i = 1; i < 7; i++ ) {
+      var precent = 200 * i - 100
+      new ScrollMagic.Scene({triggerElement: "#start_model", triggerHook: 0, duration: precent+"%"})
+      .setPin("#model"+i)
+      .addTo(controller);
+    }
 
 })
 
