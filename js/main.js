e8393c8f2940e359044b34e76057675e7fa7cc93
thehobbit85 <thehobbit85@gmail.com>
2016-07-17 05:24:52 +0300

    init commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+var controller = new ScrollMagic.Controller();
+
+$('video').click(function () {
+  this.volume == 0 ? this.volume = 1 : this.volume -= 1
+})
+
+$(function () {
+    // build scene
+    var scene = new ScrollMagic.Scene()
+    .setPin("#logo")
+    .addTo(controller);
+
+    var scene2 = new ScrollMagic.Scene()
+    .setPin("#preorder")
+    .addTo(controller);
+
+    var scene3 = new ScrollMagic.Scene({triggerElement: "#step", triggerHook: 0, duration: "350%"})
+    .setPin("#cd-vertical-nav")
+    .addTo(controller);
+
+});
