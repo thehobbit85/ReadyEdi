b110206577bd42dd8af474109bf374266def8ec6
thehobbit85 <thehobbit85@gmail.com>
2016-07-19 06:02:03 +0300

    Working (no bugs) version of the animation but not really responsive. Need to take care of it after getting the real model.

--- js/main.js
+++ js/main.js
@@ -11,6 +11,30 @@ $(function () {
     .setPin("#cd-vertical-nav")
     .addTo(controller);
 
+    var scene2 = new ScrollMagic.Scene({triggerElement: "#step", triggerHook: 0, duration: "100%"})
+    .setPin("#data_num1")
+    .addTo(controller);
+
+    var scene3 = new ScrollMagic.Scene({triggerElement: "#top_plate", triggerHook: 0, duration: "100%"})
+    .setPin("#data_num2")
+    .addTo(controller);
+
+    var scene4 = new ScrollMagic.Scene({triggerElement: "#screws", triggerHook: 0, duration: "100%"})
+    .setPin("#data_num3")
+    .addTo(controller);
+
+    var scene5 = new ScrollMagic.Scene({triggerElement: "#pdb", triggerHook: 0, duration: "100%"})
+    .setPin("#data_num4")
+    .addTo(controller);
+
+    var scene6 = new ScrollMagic.Scene({triggerElement: "#bottom_plate", triggerHook: 0, duration: "100%"})
+    .setPin("#data_num5")
+    .addTo(controller);
+
+    var scene7 = new ScrollMagic.Scene({triggerElement: "#end_model", triggerHook: 0, duration: "100%"})
+    .setPin("#data_num6")
+    .addTo(controller);
+
     for (var i = 1; i < 7; i++ ) {
       var precent = 200 * i - 100
       new ScrollMagic.Scene({triggerElement: "#start_model", triggerHook: 0, duration: precent+"%"})
