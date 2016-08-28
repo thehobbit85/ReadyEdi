fd4807ecf756844a356c5ceedeb57304261564b3
thehobbit85 <thehobbit85@gmail.com>
2016-08-29 01:12:16 +0300

    more work

--- js/main.js
+++ js/main.js
@@ -1,8 +1,23 @@
-$('video').click(function () {
-  this.volume == 0 ? this.volume = 1 : this.volume -= 0.25
-})
+var isMuted = false;
 
-$('#preorderForm').modal({
-  backdrop: 'static',
-  keyboard: false
+$(document).ready(function() {
+
+  $('#volume').click(function () {
+    var video = document.getElementById('racingVideo')
+    if (isMuted) {
+      $('#volume').attr('src','./img/volume.svg')
+      video.volume = 1
+      isMuted = false
+    } else {
+      $('#volume').attr('src','./img/volume-mute.svg')
+      video.volume = 0
+      isMuted = true
+    }
+  })
+
+  $('#preorderForm').modal({
+    backdrop: 'static',
+    keyboard: false
+  })
+
 })
